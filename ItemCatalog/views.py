f2b454dd1799198eb8266c3a7f48a0fed4349dd0
Juanadelacuesta <juanita.dlc@gmail.com>
2016-02-18 00:17:06 -0500

    Type: Style Add style to all pages

--- ItemCatalog/views.py
+++ ItemCatalog/views.py
@@ -34,9 +34,11 @@ def section(section_id):
     
     body_section = (session.query(BodySection).
         filter(BodySection.id==section_id).one()) 
-        
+    products = (session.query(Product).filter(Product.bodysection_id==section_id)
+                    .all())  
     if request.method == 'GET': 
-        return render_template('section.html', bodysection=body_section)
+        return render_template('section.html', bodysection=body_section, 
+                                    products=products)
         
 @app.route('/section/new/', methods=['GET','POST'])
 def newBodySection():
@@ -81,12 +83,13 @@ def deleteBodySection(body_section_id):
         return render_template('deleteBodySection.html', bodysection=body_section)
         
     if request.method == 'POST':
-
         if request.form['btn'] == 'Delete':
             session.delete(body_section)
             session.commit()
-        return redirect(url_for('index'))
- #   print '\n salio \n\n'
+            return redirect(url_for('index'))
+        
+        elif request.form['btn'] == 'Cancel':
+            return redirect(url_for('section', section_id=body_section_id))
 
         
 # Views for the Products
@@ -96,27 +99,38 @@ def viewProducts():
     
     products = session.query(Product).order_by(Product.bodysection_id).all()    
     return render_template('products.html', products=products)
-        
-@app.route('/product/new/', methods=['GET','POST'])
-def newProduct():
  
+
+@app.route('/product/new/<int:section_id>', methods=['GET','POST'])
+@app.route('/product/new/', methods=['GET','POST'])
+def newProduct(section_id=None):
+    
+    sections = session.query(BodySection).all()
+    if section_id:
+        preselected_section = (session.query(BodySection).
+                                    filter(BodySection.id==section_id).one())
+    else:
+        preselected_section = section_id
+        
     form = NewProductForm(request.form)
     product = Product()
     if request.method == 'GET':
-        return render_template('newProduct.html')
+        return render_template('newProduct.html', sections=sections,
+                                ps_section=preselected_section)
         
     if request.method == 'POST' and form.validate():       
         form.populate_obj(product)
         file = request.files['picture']
         filename = secure_filename(file.filename)
         product.picture_name = IMAGES_FOLDER + filename
-        print "\n\n"
-        print product
-        print "\n\n"
         if allowed_file(filename):
             file.save(app.config['UPLOAD_FOLDER'] + filename)
             session.add(product)
             session.commit()
+            
+        if section_id:
+            return redirect(url_for('section', section_id=section_id))
+            
         return redirect(url_for('viewProducts'))    
 
 @app.route('/product/<int:product_id>/')
@@ -134,23 +148,22 @@ def editProduct(product_id):
     form = NewProductForm(request.form)
     product = (session.query(Product).
                     filter(Product.id==product_id).one())
-                    
+    sections = session.query(BodySection).all()                
     if request.method == 'GET':
-        return render_template('editProduct.html', product=product)
+        return render_template('editProduct.html', 
+                                product=product, sections= sections)
 
     if request.method == 'POST' and form.validate():
         print '/n/n/n/ post/n/n'
         if request.form['btn'] == 'Save':
-            form.populate_obj(product)
-            
+            form.populate_obj(product)           
             if request.files['picture']:
                 file = request.files['picture']
-
                 filename = secure_filename(file.filename)
                 product.picture_name = IMAGES_FOLDER + filename
                 file.save(app.config['UPLOAD_FOLDER'] + filename)
-            session.add(product)
-            session.commit()
+                session.add(product)
+                session.commit()
             return redirect(url_for('product', product_id=product_id)) 
             
         elif request.form['btn'] == 'Cancel':
@@ -175,12 +188,9 @@ def deleteProduct(product_id):
 def bodySectionJson(section_id):
     section = session.query(BodySection).filter_by(id=section_id).one()
     products = session.query(Product).filter_by(bodysection_id=section.id)
-    return jsonify(specific_products=[i.serialize for p in products])
+    return jsonify(specific_products=[p.serialize for p in products])
 
 @app.route('/product/<int:product_id>/JSON/')
 def productJson(product_id):
     product = session.query(Product).filter_by(id=product_id).one()
     return jsonify(Product_info=product.serialize)
-
-
-       
