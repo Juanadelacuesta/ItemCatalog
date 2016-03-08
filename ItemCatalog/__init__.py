0a7aab7e80e30abd59c1dc7879bca1f043ead284
Juanadelacuesta <juanita.dlc@gmail.com>
2016-03-07 23:48:14 -0500

    Type: Func Add the login with google+ functionality

--- ItemCatalog/__init__.py
+++ ItemCatalog/__init__.py
@@ -6,13 +6,22 @@
 
 #------------------------------------------------------------------------------#
 
+import string
+import random
+import json
+import requests
+import httplib2
+
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from flask import Flask
 from flask_wtf.csrf import CsrfProtect
 from flask import session as login_session
-import string
-import random
+from flask import make_response
+
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+
 from config import ProductionConfig, DevelopmentConfig
 from models import Base
 
@@ -25,6 +34,8 @@ csrf.init_app(app)
 app.config.from_object('ItemCatalog.DevelopmentConfig')
 #app.config.from_object('ItemCatalog.ProductionConfig')
 
+CLIENT_ID = json.loads(
+    open('client_secret.json', 'r').read())['web']['client_id']
 
 engine = create_engine(app.config['DATABASE_URI'])
 Base.metadata.create_all(engine)
--- ItemCatalog/views.py
+++ ItemCatalog/views.py
@@ -3,16 +3,26 @@
 # Author: Maria Clara De La Cuesta
 # Description: Views for the Items Catalog project
 #------------------------------------------------------------------------------#
+import httplib2
+import json
+import requests
+import os
+
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+
 
 from flask import render_template, url_for, request, redirect, flash, jsonify
+from flask import make_response
+from flask import session as login_session
 from urllib2 import urlopen
 from werkzeug import secure_filename
 from flask_wtf.file import FileField
-import os
 from sqlalchemy_imageattach.context import store_context
-from flask import session as login_session
+
+
 from config import id_generator
-from ItemCatalog import app, session
+from ItemCatalog import app, session, csrf, CLIENT_ID
 from models import BodySection, Product
 from forms import NewBodySectionForm, NewProductForm
 
@@ -31,14 +41,104 @@ def allowed_file(filename):
 def showLogin():
         state = id_generator(32) 
         login_session['state'] = state
-        return render_template('login.html', client_id = login_session['state'])
+        return render_template('login.html', STATE = state)
+ 
+@csrf.exempt
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter blah'), 401)
+        response.headers['Content-Type']  = 'aplication/json'
+        return response
+    
+    # Obtain authorization code
+
+    code = request.data
+    try:
+        oauth_flow = flow_from_clientsecrets('client_secret.json', scope='') 
+        oauth_flow.redirect_uri='postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+        
+    except FlowExchangeError:
+        print "dentro de error"
+        response = make_response(json.dumps('Failed to upgrade the' + 
+        'authorization code blah'), 401)
+        response.headers['Content-Type']  = 'aplication/json'
+        return response
+    
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+            % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    
+    #If the access token is invalid, abort
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user Id doesnt match"), 401)
+        response.headers['Content-Type']  = 'aplication/json'
+        return response
+        
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's blah."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(
+            json.dumps('Current user is already connected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+        
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+    
+    
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+   
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 150px; height: 150px;border-radius: 75px;' 
+    output += '-webkit-border-radius: 75px;-moz-border-radius: 75px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
     
 @app.route('/')
 @app.route('/section/')
 def index():
-    print  (app.config['IMAGES_FOLDER'])
+
     body_sections = session.query(BodySection).order_by(BodySection.name).all()    
-    return render_template('index.html', bodysections=body_sections)
+    print login_session['picture']
+    return render_template('index.html', bodysections=body_sections, image=login_session['picture'])
 
     
 @app.route('/section/<int:section_id>/')
